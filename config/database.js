3f60ec932b97d4e3ff083411667c088a771164ff
ramondorosario <ramondorosario@gmail.com>
2021-06-25 22:05:09 -0300

    deploy

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'lp-reactavancado-db'),
-        username: env('DATABASE_USERNAME', 'lp-reactavancado-strapi'),
-        password: env('DATABASE_PASSWORD', 'adminlp123'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "lp-reactavancado-db"),
+        username: env("DATABASE_USERNAME", "lp-reactavancado-strapi"),
+        password: env("DATABASE_PASSWORD", "adminlp123"),
+        ssl: env.bool("DATABASE_SSL", false),
       },
-      options: {}
+      options: {},
     },
   },
 });
