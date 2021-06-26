684da5e6be9182fadcfa8b492d39167e233a4cf2
ramondorosario <ramondorosario@gmail.com>
2021-06-25 23:38:54 -0300

    revert use import

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,4 +1,4 @@
-import { parse } from "pg-connection-string";
+const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
